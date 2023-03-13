1d0f69e374d75559251d1984eb70a50db0bbce02
Brenda-A-S <brenda.antunes16@gmail.com>
2023-03-13 16:30:09 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -18,16 +17,51 @@ public class Main {
         curso2.setCargaHoraria(4);
 
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-
         //MENTORIA
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(mentoria);
+
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+
+        //BOOTCAMP E DEVS
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-------------");
+        System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluído Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
+
+        System.out.println("-------------");
+
+        Dev devBrenda = new Dev();
+        devBrenda.setNome("Brenda");
+        devBrenda.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Brenda: " + devBrenda.getConteudosInscritos());
+        devBrenda.progredir();
+        devBrenda.progredir();
+        devBrenda.progredir();
+        System.out.println("-------------");
+        System.out.println("Conteúdos Inscritos Brenda: " + devBrenda.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Brenda: " + devBrenda.getConteudosConcluidos());
+        System.out.println("XP: " + devBrenda.calcularTotalXp());
 
     }
 }
